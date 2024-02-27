1a1f0c2076c29ca69b32f4c960d9c7e543923bef
LouisKzy <louiskrejezy@gmail.com>
2024-02-27 14:34:07 +0100

    Implement delete features

--- client/src/features/posts/PostDetails.jsx
+++ client/src/features/posts/PostDetails.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { API_URL } from "../../constants";
 function PostDetails() {
   const [post, setPost] = useState(null);
   const { id } = useParams();
+  const navigate = useNavigate();
+
   useEffect(() => {
     const fetchCurrentPost = async () => {
       try {
@@ -20,14 +22,35 @@ function PostDetails() {
     };
     fetchCurrentPost();
   }, [id]);
+
+  const deletePost = async () => {
+    try {
+      const response = await fetch(`${API_URL}/${id}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        navigate("/");
+      } else {
+        throw response;
+      }
+    } catch (error) {
+      console.log("Error: " + error);
+    }
+  };
+
   if (!post) {
     return <p>Loading...</p>;
   }
+
   return (
     <div>
       <h2>{post.title}</h2>
       <p>{post.body}</p>
       <Link to="/">Back to posts</Link>
+      {" | "}
+      <Link to={`/posts/${id}/edit`}>Edit post</Link>
+      {" | "}
+      <button onClick={deletePost}>Delete</button>
     </div>
   );
 }
