beed5450c06e469daef1f6c14f4611559629f335
LouisKzy <louiskrejezy@gmail.com>
2024-02-27 13:56:13 +0100

    Implemented edit

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import EditPostForm from "../features/posts/EditPostForm";
 import NewPostForm from "../features/posts/NewPostForm";
 import PostDetails from "../features/posts/PostDetails";
 import PostsList from "../features/posts/PostsList";
@@ -7,6 +8,7 @@ function AppRoutes() {
     <Routes>
       <Route path="/" element={<PostsList />} />
       <Route path="posts/:id" element={<PostDetails />} />
+      <Route path="posts/:id/edit" element={<EditPostForm />} />
       <Route path="/new" element={<NewPostForm />} />
     </Routes>
   );
